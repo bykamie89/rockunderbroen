e420d4b1539aba4f6acd71a86bcecb4444e6a893
Kamilla <kamillakkngmail.com>
2023-05-30 13:35:11 +0200

    more dlc

--- src/scripts/overviewDropdown.js
+++ src/scripts/overviewDropdown.js
@@ -1,19 +1,21 @@
-var info = document.getElementsByClassName('infobox');
+document.addEventListener('load', () => {
+  var info = document.getElementsByClassName('infobox');
 
-var i;
+  var i;
 
-for (i = 0; i < info.length; i++) {
-  info[i].addEventListener('click', function () {
-    this.classList.toggle('active');
-    var content = this.nextElementSibling;
-    let imgelement = this.querySelector('.testhall');
-    console.log();
-    if (content.style.display === 'block') {
-      content.style.display = 'none';
-      imgelement.classList.remove('rotate');
-    } else {
-      content.style.display = 'block';
-      imgelement.classList.add('rotate');
-    }
-  });
-}
+  for (i = 0; i < info.length; i++) {
+    info[i].addEventListener('click', function () {
+      this.classList.toggle('active');
+      var content = this.nextElementSibling;
+      let imgelement = this.querySelector('.testhall');
+      console.log();
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+        imgelement.classList.remove('rotate');
+      } else {
+        content.style.display = 'block';
+        imgelement.classList.add('rotate');
+      }
+    });
+  }
+});
