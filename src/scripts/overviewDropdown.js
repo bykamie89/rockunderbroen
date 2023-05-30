d034a1a13aa0235fab8a737e15d02951caeb9a8f
Kamilla <kamillakkngmail.com>
2023-05-30 14:00:28 +0200

    updated js

--- src/scripts/overviewDropdown.js
+++ src/scripts/overviewDropdown.js
@@ -1,21 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   var info = document.getElementsByClassName('infobox');
+  if (info) {
+    var i;
 
-  var i;
-
-  for (i = 0; i < info.length; i++) {
-    info[i].addEventListener('click', function () {
-      this.classList.toggle('active');
-      var content = this.nextElementSibling;
-      let imgelement = this.querySelector('.testhall');
-      console.log();
-      if (content.style.display === 'block') {
-        content.style.display = 'none';
-        imgelement.classList.remove('rotate');
-      } else {
-        content.style.display = 'block';
-        imgelement.classList.add('rotate');
-      }
-    });
+    for (i = 0; i < info.length; i++) {
+      info[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        var content = this.nextElementSibling;
+        let imgelement = this.querySelector('.testhall');
+        console.log();
+        if (content.style.display === 'block') {
+          content.style.display = 'none';
+          imgelement.classList.remove('rotate');
+        } else {
+          content.style.display = 'block';
+          imgelement.classList.add('rotate');
+        }
+      });
+    }
   }
 });
