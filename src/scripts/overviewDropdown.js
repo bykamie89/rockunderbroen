dcd7e91caa163845a140fc96fea8ddd7dc3ae58c
Kamilla <kamillakkngmail.com>
2023-05-30 10:20:46 +0200

    overview dropdown push

--- src/scripts/overviewDropdown.js
+++ src/scripts/overviewDropdown.js
@@ -1,21 +1,19 @@
-const closeArrow = document.querySelector('.closeArrow');
-const openArrow = document.querySelector('.openArrow');
-const content = document.querySelector('.content');
-
 var info = document.getElementsByClassName('infobox');
+
 var i;
 
 for (i = 0; i < info.length; i++) {
   info[i].addEventListener('click', function () {
     this.classList.toggle('active');
+    var content = this.nextElementSibling;
+    let imgelement = this.querySelector('.testhall');
+    console.log();
     if (content.style.display === 'block') {
       content.style.display = 'none';
-      openArrow.style.display = 'block';
-      closeArrow.style.display = 'none';
+      imgelement.classList.remove('rotate');
     } else {
       content.style.display = 'block';
-      openArrow.style.display = 'none';
-      closeArrow.style.display = 'block';
+      imgelement.classList.add('rotate');
     }
   });
 }
