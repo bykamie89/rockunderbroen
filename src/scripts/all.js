1be5874fa3581845222e65ab5ad9d3db5701e5b7
Kamilla <kamillakkngmail.com>
2023-06-01 10:46:31 +0200

    cookie box implemented

--- src/scripts/all.js
+++ src/scripts/all.js
@@ -78,3 +78,21 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const cookieBox = document.querySelector('.cookiewrapper'),
+    acceptBtn = cookieBox.querySelector('.cookiebuttons button');
+
+  acceptBtn.onclick = () => {
+    document.cookie = 'CookieBy=Vkive; max-age=' + 60 * 60 * 24 * 30;
+    if (document.cookie) {
+      cookieBox.classList.add('hide');
+    } else {
+      alert("Cookie can't be set!");
+    }
+  };
+  let checkCookie = document.cookie.indexOf('CookieBy=Vkive');
+  checkCookie != -1
+    ? cookieBox.classList.add('hide')
+    : cookieBox.classList.remove('hide');
+});
